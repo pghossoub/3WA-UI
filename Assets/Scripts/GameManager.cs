fdd9cd1a7051db827a1ffe81f874fc258dff3fd8
unknown <pghossoub@hotmail.fr>
2020-02-18 17:28:24 +0100

    Reorganise Script folder for package. Mouse Cursor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Event myEvent;
+
+    public Texture2D m_texture;
+
+    public void Awake()
+    {
+        Cursor.SetCursor(m_texture, new Vector2(0,1), CursorMode.Auto);
+    }
     public void Response()
     {
         Debug.Log("Response to Event !");
